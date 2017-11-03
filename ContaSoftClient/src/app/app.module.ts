48f0031c1b90edc790b0b1cc766804a4a2333b78
Carlos <charliesr2@gmail.com>
2017-11-03 01:10:19 +0100

    TopBar created

--- ContaSoftClient/src/app/app.module.ts
+++ ContaSoftClient/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopBarComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule
